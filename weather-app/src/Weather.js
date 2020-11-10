edf9c36ef6db18cc6914e921eaff62f0f28a5bca
Taru Airola <taru.airola@wunderdog.fi>
2020-11-11 00:01:13 +0200

    Mahdollisuus vaihtaa fokusoitua sää-kuvaketta

--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -7,12 +7,16 @@ class Weather extends Component {
     super(props)
     this.state = {
       forecasts: null,
+      focusedForecastDt: null
     }
   }
 
   async componentDidMount() {
     // Haetaan data ensimmäisellä mountilla
     await this.getForecast()
+    // Asetetaan aluksi oletuksena valituksi sääksi listan ensimmäinen sää,
+    // tunnistetaan se dt eli datetimen avulla joten tallennetaan se
+    this.setState(prevState => ({focusedForecastDt: prevState.forecasts.list[0].dt}))
     // Asetetaan intervalli tunnin välein hakemaan päivitetty data
     // 1 tunti on 60*60*1000 = 3600000 millisekuntia
     setInterval(async () => {
@@ -41,9 +45,13 @@ class Weather extends Component {
     return `${hours}:${minutes}`
   }
 
+  changeFocusedForecast = forecast => {
+    this.setState({focusedForecastDt: forecast.dt})
+  }
+
   renderWeatherItem(forecast) {
     return (
-      <div className="Weather_box" key={forecast.dt}>
+      <div className="Weather_box" key={forecast.dt} onClick={() => this.changeFocusedForecast(forecast)}>
         <div className="Weather_day">{this.formatDay(forecast.dt)}</div>
         <div className="Weather_time">{this.formatTime(forecast.dt)}</div>
         <div className="Weather_temp">{Math.round(forecast.main.temp)}°</div>
@@ -53,14 +61,25 @@ class Weather extends Component {
   }
 
   render() {
-    if (!this.state.forecasts) return null
-    const forecasts = this.state.forecasts
+    const {forecasts, focusedForecastDt} = this.state
+    if (!forecasts || !focusedForecastDt) return null
+    const selectedForecast = forecasts.list.find(f => f.dt === focusedForecastDt)
     return (
       <div className="Weather">
+        <div className="Weather_city">
+          {forecasts.city.name}
+        </div>
+        <div className="Weather_sunrise">
+          Aurinko nousee klo {this.formatTime(forecasts.city.sunrise)}
+        </div>
+        <div className="Weather_sunset">
+          Aurinko laskee klo {this.formatTime(forecasts.city.sunset)}
+        </div>
         <div className="Weather_selected">
-          <div className="Weather_day">{this.formatDay(forecasts.list[0].dt)}</div>
-          <div className="Weather_time">{this.formatTime(forecasts.list[0].dt)}</div>
-          <div className="Weather_temp">{Math.round(forecasts.list[0].main.temp)}°</div>
+          <div className="Weather_day">{this.formatDay(selectedForecast.dt)}</div>
+          <div className="Weather_time">{this.formatTime(selectedForecast.dt)}</div>
+          <div className="Weather_temp">{Math.round(selectedForecast.main.temp)}°</div>
+          <div className="Weather_desc">{selectedForecast.weather[0].description}</div>
           <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
         </div>
         <div className="Weather_forecast">
